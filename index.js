7b3aca85c94a5bb23e0ff26ec7f52fe7731573e4
vijaysudarsanam98 <51353941+vijaysudarsanam98@users.noreply.github.com>
2022-07-20 15:21:42 +0530

    server_files

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser');
+const app = express();
+
+
+
+//initialize sentry logging
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+//default GET request
+app.get(['/', '/health'], function (req, res) {
+    res.send('node is up');
+    
+
+});
+
+
+
+
+
+
+
+app.listen(port, async function () {
+    console.log(`project is up: ${process.env.NODE_ENV}`);
+ 
+
+
+
+ 
+ 
+   
+
+});
+
+
+
